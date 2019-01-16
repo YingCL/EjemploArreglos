50a1cf0151a22031b841e086fcf97394c15d0780
anonimo <anonimo@MARCELODESK>
2019-01-16 04:07:15 -0300

    VERSION 1

--- src/com/marcelo/arreglos/Persona.java
+++ src/com/marcelo/arreglos/Persona.java
@@ -17,7 +17,7 @@ public class Persona {
 
     @Override
     public String toString() {
-        return "Persona{" + "nombre=" + getNombre() + "}";
+        return "Persona {" + "nombre=" + getNombre() + "}";
     }
     
 }
